b9d466dcd1785be089fb58c806a8ad79e86bea7e
Ganiev-Rustem <rusyfox@gmail.com>
2026-07-01 03:30:35 +0500

    add alias

--- api/menu.tsx
+++ api/menu.tsx
@@ -1,7 +1,7 @@
 import { API } from '@/app/api';
 import type { MenuItem } from '@/interfaces/menu.interface';
 
-export async function getMenu(firstCategory: number): Promise<MenuItem[]> {
+export async function getMenu(firstCategory: number): Promise<MenuItem[] > {
 	const res = await fetch(API.topPage.find, {
 		method: 'POST',
 		body: JSON.stringify({
@@ -9,5 +9,6 @@ export async function getMenu(firstCategory: number): Promise<MenuItem[]> {
 		}),
 		headers: new Headers({ 'content-type': 'application/json'})
 	});
+
 	return res.json();
 } 
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 
 import { getMenu } from '@/api/menu';
 
-
+ 
 export default async function Home() {
 	const menu = await getMenu(0);
 	return (
